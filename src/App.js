253968ec7658760572c287a8b166c7aab54d6ac1
Ravi-Pathak-demo <ravipathak934@gmail.com>
2022-04-13 09:47:12 +0530

    commit
    monster rolodex

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list";
+import { SearchBox } from "./components/search-box/search-box";
+
+// or Class App extends React.Component if {Component not included in import}
+class App extends Component {
+  com;
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filterMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+      <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filterMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
